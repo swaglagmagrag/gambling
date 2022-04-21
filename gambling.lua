82854a1fe535c4d3bf3204e6d28ce62c78e146a3
swaglagmagrag <75270704+swaglagmagrag@users.noreply.github.com>
2022-04-21 11:26:01 -0500

    Changes

--- gambling.lua
+++ gambling.lua
@@ -1,6 +1,8 @@
-local money = 10 
-local name = "" 
-local helped = false 
+money = 10 
+name = "" 
+helped = false 
+a50 = true 
+returning = false 
 
 function begin() 
   local a = "Hello, welcome to my gambling site! Before we do anything, what's your name?" 
@@ -8,6 +10,7 @@ function begin()
   local c = {"Snake Eyes", "50/50", "Blackjack"} 
   local d = "Welcome back " .. name .. "! Where would you like to go?" 
   local e = "So, where would you like to go?" 
+  local f = "Oooh, looks like you're out of money! You can earn more by typing mines."
   if name == "" then 
     print(a) 
     name = io.read()
@@ -15,8 +18,11 @@ function begin()
   elseif helped == true then 
     print(e) 
     helped = false 
-  else 
+  elseif returning == false then 
     print(b) 
+  else
+    print(d)
+    returning = false
   end 
   local i = io.read() 
   if i == "help" then 
@@ -32,45 +38,129 @@ function begin()
     helped = true 
     return begin()
   elseif i == "50/50" then
-    return 50/50()
-  elseif i == "return"
+    return a5050()
+  elseif i == "Snake Eyes" or i == "snake eyes" then 
+    return snake1() 
+  elseif i == "mines" then
+    return mines()
+  elseif i == "return" then
     print("Thank you for visiting!")
     return 
+  else
+    print("Sorry, I couldn't understand that.")
+    return begin()
   end 
 end
 
-function 50/50() 
-   local first = true 
+function a5050() 
+   if money == 0 then 
+     print("Sorry, you can't play here if you don't have any money!") 
+     return begin()
+   end 
    local a = "Welcome to 50/50! If you need any help playing, just let me know!" 
    local b = "So, how much are you gonna gamble?" 
-   if first == true then 
+   if a50 == true then 
     print(a) 
-    first = false 
-    return 50/502() 
+    a50 = false 
+    return b5050() 
    else 
     print(b) 
-    return 50/502() 
+    return b5050() 
    end 
 end 
 
-function 50/502() 
+function b5050() 
   local a = "Congrats, you won " 
   local b = "Sorry, you lost " 
-  local i = tonumber(io.read()) 
-  if i == nil or i < 0 then 
-   print("Please use a number, and make sure its above 0!") 
-  elseif i > money then 
+  local i = io.read()
+  local f = tonumber(i)
+  if i == "help" then 
+      print("You type the amount you would like to gamble, and if you have enough, we will do a coinflip for it! When you want to go back to the main lobby, just type back.")
+      return a5050() 
+  elseif i == "back" then
+      returning = true
+      return begin()
+  elseif f == nil or f <= 0 then 
+   print("Please use a number, and make sure its above 0!")
+   return a5050() 
+  elseif f > money then 
     print("Please dont try to gamble more than you have!") 
-    return 50/502() 
+    return a5050()   
   end 
   local c = math.random(2) 
   local pl = 1 
   if c ~= pl then 
-    money = money - i 
-    print(b .. i .. ".") 
+    money = money - f
+    print(b .. "$" .. f .. "! You now have $" .. money .. ".") 
   else 
-    money = money + i 
-    print(a .. i .. ".") 
+    money = money + f 
+    print(a .. "$" .. f .. "! You now have $" .. money .. ".") 
   end 
-  return 50/50() 
+  return a5050() 
 end 
+
+local returning = false
+
+function snake1() 
+    if money == 0 then 
+     print("Sorry, you can't play here if you don't have any money!") 
+     return begin() 
+    end 
+    local win = "Congrats, you won " 
+    local lose = "Sorry, you lost " 
+    local a = "Welcome to snake eyes! You bet the amount you want, and if both dice land on 1's, then you get 12 times what you put in!" 
+    local b = "So, how much would you like to gamble?" 
+    if returning == false then 
+        print(a .. " " .. b) 
+    end 
+    local i = io.read() 
+    local f = tonumber(i) 
+    if i == "help" then 
+        print("There ain't much more I can help you with here! If you'd like to go back to the main area, just type back") 
+        returning = true 
+        return snake1() 
+    elseif i == "back" then 
+        begin() 
+    elseif f == nil or f <= 0 then 
+      print("Please use a number, and make sure its above 0!") 
+      returning = true 
+      return snake1() 
+    elseif f > money then 
+      print("Please dont try to gamble more than you have!") 
+      returning = true 
+      return snake1() 
+    end 
+    local c = math.random(6) 
+    local d = math.random(6) 
+    if c == 1 and d == 1 then 
+        money = money + (f * 12) 
+        print(win .. "$" .. f * 12 .. "! You now have $" .. money .. ".") 
+        return snake1() 
+    else 
+        money = money - f
+        print(lose .. "$" .. f .. "! You now have $" .. money .. ".")  
+        return snake1() 
+    end 
+end 
+firsttime = true 
+function mines() 
+  local a = "Welcome to the mines " .. name .. "! Here, you have to type the letter than shows up on the screen, each letter you get correct will give you $1."
+  local b = "Welcome back " .. name .. "!"
+  local c = "Not much more I can help you with here, just type the correct letters to earn some extra cash! Type back when you would like to return"
+  if firsttime == true then
+    print(a)
+    firsttime = false
+  else
+    print(b)
+  end
+  local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=+_)(*&^%$#@!/.,<>?';:{}|"
+  local r = math.random(#chars)
+  local c = chars:sub(r,r)
+  local i = io.read()
+  if i == "back" then
+    return begin()
+  elseif i == c then
+    money = money + 1
+    print("Correct! You have earned 1 dollar, and you are now back at $" .. money)
+  end
+end
