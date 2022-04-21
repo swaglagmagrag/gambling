706eb5724f96eb6a060619bd143a886085f09d95
swaglagmagrag <75270704+swaglagmagrag@users.noreply.github.com>
2022-04-21 12:57:11 -0500

    Gamble gamble
    <3

--- gambling.lua
+++ gambling.lua
@@ -1,8 +1,9 @@
-money = 10 
+money = 100
 name = "" 
 helped = false 
 a50 = true 
 returning = false 
+can = false 
 
 function begin() 
   local a = "Hello, welcome to my gambling site! Before we do anything, what's your name?" 
@@ -10,19 +11,24 @@ function begin()
   local c = {"Snake Eyes", "50/50", "Blackjack"} 
   local d = "Welcome back " .. name .. "! Where would you like to go?" 
   local e = "So, where would you like to go?" 
-  local f = "Oooh, looks like you're out of money! You can earn more by typing mines."
+  if money == 0 then
+    can = true
+    return mines()
+  end
   if name == "" then 
     print(a) 
-    name = io.read()
+    name = io.read() 
     return begin() 
   elseif helped == true then 
     print(e) 
     helped = false 
   elseif returning == false then 
     print(b) 
+  elseif returning == true then
+    print(e)
   else
-    print(d)
-    returning = false
+    print(d) 
+    returning = false 
   end 
   local i = io.read() 
   if i == "help" then 
@@ -36,26 +42,29 @@ function begin()
       end 
     end 
     helped = true 
-    return begin()
-  elseif i == "50/50" then
-    return a5050()
+    return begin() 
+  elseif i == "50/50" then 
+    return a5050() 
   elseif i == "Snake Eyes" or i == "snake eyes" then 
-    return snake1() 
-  elseif i == "mines" then
-    return mines()
-  elseif i == "return" then
-    print("Thank you for visiting!")
-    return 
-  else
-    print("Sorry, I couldn't understand that.")
+    return snake1()
+  elseif i == "Blackjack" or i == "blackjack" then
+    print("Sorry, I lost my cards for now! Come back later and I might find more.")
+    returning = true
     return begin()
+  elseif i == "return" or i == "back" or i == "leave" or i == "stop" then 
+    print("Thank you for visiting!") 
+    return 
+  else 
+    print("Sorry, I couldn't understand that.") 
+    helped = true
+    return begin() 
   end 
-end
+end 
 
 function a5050() 
    if money == 0 then 
      print("Sorry, you can't play here if you don't have any money!") 
-     return begin()
+     return mines() 
    end 
    local a = "Welcome to 50/50! If you need any help playing, just let me know!" 
    local b = "So, how much are you gonna gamble?" 
@@ -72,25 +81,29 @@ end
 function b5050() 
   local a = "Congrats, you won " 
   local b = "Sorry, you lost " 
-  local i = io.read()
-  local f = tonumber(i)
+  local i = io.read() 
+  local f = tonumber(i) 
   if i == "help" then 
-      print("You type the amount you would like to gamble, and if you have enough, we will do a coinflip for it! When you want to go back to the main lobby, just type back.")
+      print("You type the amount you would like to gamble, and if you have enough, we will do a coinflip for it! When you want to go back to the main lobby, just type back.") 
       return a5050() 
-  elseif i == "back" then
-      returning = true
-      return begin()
+  elseif i == "back" then 
+      returning = true 
+      return begin() 
   elseif f == nil or f <= 0 then 
-   print("Please use a number, and make sure its above 0!")
+   print("Please use a number, and make sure its above 0!") 
    return a5050() 
   elseif f > money then 
     print("Please dont try to gamble more than you have!") 
-    return a5050()   
+    return a5050() 
+  end
+  if f > 20 and money > f then
+    print("Sorry, there is a maximum bet of $20 at this location!") 
+    return a5050()
   end 
   local c = math.random(2) 
   local pl = 1 
   if c ~= pl then 
-    money = money - f
+    money = money - f 
     print(b .. "$" .. f .. "! You now have $" .. money .. ".") 
   else 
     money = money + f 
@@ -99,12 +112,12 @@ function b5050()
   return a5050() 
 end 
 
-local returning = false
+local returning = false 
 
 function snake1() 
     if money == 0 then 
      print("Sorry, you can't play here if you don't have any money!") 
-     return begin() 
+     return mines() 
     end 
     local win = "Congrats, you won " 
     local lose = "Sorry, you lost " 
@@ -143,24 +156,39 @@ function snake1()
     end 
 end 
 firsttime = true 
+stillmines = false 
 function mines() 
-  local a = "Welcome to the mines " .. name .. "! Here, you have to type the letter than shows up on the screen, each letter you get correct will give you $1."
-  local b = "Welcome back " .. name .. "!"
-  local c = "Not much more I can help you with here, just type the correct letters to earn some extra cash! Type back when you would like to return"
-  if firsttime == true then
-    print(a)
-    firsttime = false
+  local a = "Welcome to the mines " .. name .. "! Here, you have to type the character than shows up on the screen, each letter you get correct will give you $1." 
+  local b = "Welcome back " .. name .. "!" 
+  local c = "Not much more I can help you with here, just type the correct characters to earn some extra cash! Type back when you would like to return" 
+  if stillmines == false then
+    if firsttime == true then 
+      print(a) 
+      firsttime = false 
+    else 
+      print(b) 
+    end
+  end 
+  local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=+_)(*&^%$#@!/.,<>?';:{}|" 
+  local r = math.random(#chars) 
+  local c = chars:sub(r,r) 
+  print(c)
+  local i = io.read() 
+  if i == "back" then 
+    return begin() 
+  elseif i == c then 
+    money = money + 1 
+    print("Correct! You have earned 1 dollar, and you are now back at $" .. money) 
+    stillmines = true 
+    return mines() 
   else
-    print(b)
-  end
-  local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=+_)(*&^%$#@!/.,<>?';:{}|"
-  local r = math.random(#chars)
-  local c = chars:sub(r,r)
-  local i = io.read()
-  if i == "back" then
-    return begin()
-  elseif i == c then
-    money = money + 1
-    print("Correct! You have earned 1 dollar, and you are now back at $" .. money)
-  end
-end
+    if money == 0 then
+      print("Since you still don't have any money, I'll give you a second chance.")
+      return mines()
+    else
+      print("Oops! You got the character wrong, back to the front with you!") 
+      return begin()
+    end
+  end 
+end 
+begin()
